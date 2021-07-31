74cb38bdb30cfacb380856388b88b379c8e21e7d
Trey Martin <treykaws@gmail.com>
2021-07-31 01:53:12 -0400

    continued prisma setup

--- src/plugins/server/handlers/user.ts
+++ src/plugins/server/handlers/user.ts
@@ -1,7 +1,11 @@
-export default function (app: any, opts: any, next: any) {
-  app.get('/', (request: any, reply: any) => {
-    reply.send({ route: 'Get User' })
+import { FastifyInstance } from 'fastify'
+
+export default function (app: FastifyInstance, opts: any, next: any) {
+  app.get('/', async (request: any, reply: any) => {
+    const getUser = await app.prisma.user.count()
+    reply.send({ userCount: getUser })
   })
+
   app.post('/', (request: any, reply: any) => {
     reply.send({ route: 'Create User' })
   })
