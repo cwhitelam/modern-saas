251b196181de955197c1260b1c51f556d455caa7
Trey Martin <treykaws@gmail.com>
2021-07-31 00:29:27 -0400

    pluginize backend and ssr server

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
 import Fastify from 'fastify'
-import fastifyNextjs from 'fastify-nextjs'
-import server from './server/'
+import next from './plugins/next/'
+import server from './plugins/server/'
 
 export default async function app() {
   const app = Fastify({ logger: true, pluginTimeout: 20000 })
 
-  app
-    .register(fastifyNextjs, {
-      logLevel: 'debug',
-      noServeAssets: false
-    })
-    .after((e) => {
-      console.log(e)
-      app.next('*')
-    })
+  // Encapsulate nextjs SSR implementation
+  app.register(next)
 
   // Encapsulate backend sever implementation
   app.register(server)
---
+++ src/plugins/next/index.ts
@@ -0,0 +1,16 @@
+import { FastifyError } from 'fastify'
+import fastifyNextjs from 'fastify-nextjs'
+
+export default function (app: any, opts: any, next: any) {
+  app
+    .register(fastifyNextjs, {
+      logLevel: 'debug',
+      noServeAssets: false
+    })
+    .after((e: FastifyError) => {
+      console.log(e)
+      app.next('*')
+    })
+
+  next()
+}
