3acbb46a8fd6142424e10c435323e958dc674b61
Trey Martin <treykaws@gmail.com>
2021-08-01 16:59:19 -0400

    add next build step for prod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Nav from '@components/Nav'
 import MSCard from '@components/ui/MSCard'
 import { internalFetcher } from '@utils/fetchers'
-import useSWR from 'swr'
 
 interface IndexPageProps {
   data: { health: string }
 }
 
 export default function IndexPage(props: IndexPageProps) {
-  const initialData = props.data
-  const { data } = useSWR('/api/health', internalFetcher, { initialData })
-
   return (
     <div className="mx-auto max-w-screen-lg">
       <Nav />
