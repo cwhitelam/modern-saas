79e569242e967f88d4d16867440c2afc43488214
Trey Martin <treykaws@gmail.com>
2021-08-01 11:39:48 -0400

    revert all tests for time being

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest'
-  },
-  testEnvironment: 'node',
-  testRegex: './src/plugins/server/tests.*\\.(test|spec)?\\.(ts|ts)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/src']
-}
--- src/plugins/server/tests/index.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('test', () => {
-  test('add', async () => {
-    expect(1 + 1).toEqual(2)
-  })
-})
-
-export {}
