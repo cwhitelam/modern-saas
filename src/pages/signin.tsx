e188c58e93e1f67b3b0cf9e4d59af9a520a089f8
Trey Martin <treykaws@gmail.com>
2021-08-01 23:51:56 -0400

    fix page structure

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -3,13 +3,15 @@ import MSCard from '@components/ui/MSCard'
 
 export default function SignIn() {
   return (
-    <div className="mx-auto max-w-screen-lg">
+    <div>
       <Nav />
-      <div className="mt-8 mx-2 md:mx-0">
-        <MSCard className="w-full md:w-1/2 mx-2 md:mx-auto">
-          <h3 className="text-3xl font-bold">Sign in</h3>
-          Sign in form
-        </MSCard>
+      <div className="mx-auto max-w-screen-lg">
+        <div className="mt-8 mx-2 md:mx-0">
+          <MSCard className="w-full md:w-1/2 mx-2 md:mx-auto">
+            <h3 className="text-3xl font-bold">Sign in</h3>
+            Sign in form
+          </MSCard>
+        </div>
       </div>
     </div>
   )
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,13 +3,15 @@ import MSCard from '@components/ui/MSCard'
 
 export default function SignupPage() {
   return (
-    <div className="mx-auto max-w-screen-lg">
+    <div>
       <Nav />
-      <div className="mt-8 mx-2 md:mx-0">
-        <MSCard className="w-full md:w-1/2 mx-2 md:mx-auto">
-          <h3 className="text-3xl font-bold">Sign up</h3>
-          Sign up form
-        </MSCard>
+      <div className="mx-auto max-w-screen-lg">
+        <div className="mt-8 mx-2 md:mx-0">
+          <MSCard className="w-full md:w-1/2 mx-2 md:mx-auto">
+            <h3 className="text-3xl font-bold">Sign up</h3>
+            Sign up form
+          </MSCard>
+        </div>
       </div>
     </div>
   )
