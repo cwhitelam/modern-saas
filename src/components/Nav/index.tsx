4da7d96184579f40d83c374879795f8b5e4f6b78
Trey Martin <treykaws@gmail.com>
2021-08-01 22:30:06 -0400

    add util methods and some minor styling

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,21 +1,32 @@
 import MSButton from '@components/ui/MSButton'
+import { MEDIA_QUERIES } from '@utils/mediaQueries'
+import useMediaQuery from '@utils/useMediaQuery'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function Nav() {
   const router = useRouter()
+  const check = useMediaQuery(MEDIA_QUERIES.SMALL)
+  const [isNotSmall, setIsNotSmall] = useState(false)
+
+  useEffect(() => {
+    setIsNotSmall(check || false)
+  }, [check])
 
   return (
-    <div className="flex items-center justify-between px-4 py-4 mt-4">
-      <div>
-        <Link href="/">
-          <a className="font-bold text-lg">Modern SaaS</a>
-        </Link>
-      </div>
-      <div className="flex items-center space-x-1">
-        <MSButton flat="true">Sign in</MSButton>
+    <div className="border-b border-grey-100">
+      <div className="mx-auto max-w-screen-lg flex  items-center justify-between px-2 py-4 mt-6">
+        <div>
+          <Link href="/">
+            <a className="font-bold text-lg">Modern SaaS {isNotSmall ? 'large' : 'small'} </a>
+          </Link>
+        </div>
+        <div className="flex items-center space-x-1">
+          <MSButton flat="true">Sign in</MSButton>
 
-        <MSButton onClick={() => router.push(`/signup`)}>Sign up for free</MSButton>
+          <MSButton onClick={() => router.push(`/signup`)}>Sign up for free</MSButton>
+        </div>
       </div>
     </div>
   )
--- src/components/ui/MSButton/MSButton.tsx
+++ src/components/ui/MSButton/MSButton.tsx
@@ -1,7 +1,7 @@
 const MSButton = ({ ...props }) => {
   return (
     <button
-      className={`font-bold py-2 px-4 border border-transparent rounded transition delay-75 duration-300 ease-in-out
+      className={`text-sm font-bold py-2 px-4 border border-transparent rounded transition delay-75 duration-300 ease-in-out
         ${
           props.flat
             ? `bg-transparent text-blue-500 hover:border hover:border-blue-500`
