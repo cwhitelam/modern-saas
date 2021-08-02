0ed4ccabca5a36fd3811113f5c26a9656b19c7d4
Trey Martin <treykaws@gmail.com>
2021-08-02 19:08:09 -0400

    style changes

--- src/components/ui/MSCard/MSCard.tsx
+++ src/components/ui/MSCard/MSCard.tsx
@@ -1,5 +1,9 @@
 const MSCard = (props: any) => {
-  return <div className={`rounded overflow-hidden shadow-lg p-4 `}>{props.children}</div>
+  return (
+    <div className={`${props.className} rounded overflow-hidden shadow-lg p-4 `}>
+      {props.children}
+    </div>
+  )
 }
 
 export default MSCard
