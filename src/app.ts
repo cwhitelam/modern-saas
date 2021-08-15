e69a8d12ab1115742f55fedd778a6017bfac928a
Trey Martin <treykaws@gmail.com>
2021-08-15 00:10:13 -0400

    add swagger to dev env

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import Fastify, { FastifyInstance, FastifyServerOptions } from 'fastify'
 import { HttpErrors } from 'fastify-sensible/lib/httpError'
+import { OpenAPIV3 } from 'openapi-types'
 import next from './plugins/next'
 import prisma from './plugins/prisma'
 import server from './plugins/server'
 
 declare module 'fastify' {
   interface FastifyInstance {
+    swagger: (opts?: { yaml?: boolean }) => OpenAPIV3.Document
+
+    swaggerCSP: {
+      script: string[]
+      style: string[]
+    }
     httpErrors: HttpErrors
   }
 }
--- src/plugins/server/index.ts
+++ src/plugins/server/index.ts
@@ -4,7 +4,25 @@ import handlers from './handlers'
 export default function (app: FastifyInstance, opts: any, next: any) {
   app.register(require('fastify-sensible'))
 
+  app.register(require('fastify-swagger'), {
+    routePrefix: '/api/docs',
+    exposeRoute: true,
+    swagger: {
+      info: {
+        title: 'Modern SaaS'
+      }
+    }
+  })
+
   app.register(handlers, { prefix: '/api' })
+
+  app.ready((err) => {
+    if (err) throw err
+
+    if (process.env.NODE_ENV !== 'production') {
+      app.swagger()
+    }
+  })
 
   next()
 }
