9e05fde23fa4b9d0a8ff2ee27fff883ba7d795df
Trey Martin <treykaws@gmail.com>
2021-09-08 20:11:38 -0400

    pwa patch to avoid unecessary warnings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    register: true,
+    disable: !isProd,
     runtimeCaching
   }
 })
