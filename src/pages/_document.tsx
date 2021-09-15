37b3b4b3b8aacc569271b3c1afd535d8c8cb4aa7
Trey Martin <treykaws@gmail.com>
2021-09-15 18:32:54 -0400

    pwa optimizations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,8 +25,7 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            maximum-scale="5"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
           />
           <meta name="description" content="Description" />
           <meta name="keywords" content="Keywords" />
